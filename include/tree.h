a2e4b8428f9476bad62cea259d6314146a218947
Balanar300hp <kudashov.vlad@yandex.ru>
2016-05-17 00:56:51 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -4,61 +4,68 @@ using namespace std;
 
 
 template <class T>
-	class Tree;
+class Tree;
 
 template <class T>
-ostream & operator<<(ostream & out,Tree<T> &tree);
+ostream & operator<<(ostream & out, Tree<T> &tree);
 
 template <class T>
-ofstream & operator<<(ofstream & fout,Tree<T> &tree);
+ofstream & operator<<(ofstream & fout, Tree<T> &tree);
 
 template <class T>
-ifstream & operator >> (ifstream & fin,Tree<T> &tree);
+ifstream & operator >> (ifstream & fin, Tree<T> &tree);
 
 
-	//Класс исключений 
+//Класс исключений 
 class Exceptions {
 	char* err;
 public:
 	Exceptions(char* _err);
 	char* what();
 };
-class File_Not_Open : public Exceptions{
+class File_Not_Open : public Exceptions {
 public:
 	File_Not_Open();
 };
-class Empty : public Exceptions{
+class Empty : public Exceptions {
 public:
 	Empty();
 };
-class Exist : public Exceptions{
+class Exist : public Exceptions {
 public:
 	Exist();
 };
-Exceptions::Exceptions(char* _err) : err(_err){}
+class Deleted : public Exceptions {
+public:
+	Deleted();
+};
+
+Exceptions::Exceptions(char* _err) : err(_err) {}
 char* Exceptions::what() { return err; }
 Exist::Exist() : Exceptions("ERROR: It's already exist!") {}
 File_Not_Open::File_Not_Open() : Exceptions("ERROR: file is not open!") {}
 Empty::Empty() : Exceptions("ERROR: The Binary Tree is empty!") {}
+Deleted::Deleted() : Exceptions("ERROR: It was deleted before") {}
 
 
 
 template <class T>
-class Tree{
+class Tree {
 public:
 	Tree();
 	bool Insert(T x);// Добавление элемента 
 	bool Search(T x);// Поиск элемента 
+	bool del(T x); //удаление узла дерева
 	friend ostream & operator<< <>(ostream &out, Tree<T> &tree);
-	friend ofstream & operator<< <>(ofstream &fout, Tree<T> &tree);		
-	friend ifstream & operator>> <>(ifstream &fin, Tree<T> &tree);		
+	friend ofstream & operator<< <>(ofstream &fout, Tree<T> &tree);
+	friend ifstream & operator >> <>(ifstream &fin, Tree<T> &tree);
 private:
 	class Root;
 	Root* root; //корень дерева
 };
 
 template <class T>
-class Tree<T>::Root{
+class Tree<T>::Root {
 public:
 	Root(T x);
 	void Insert(T x);
@@ -66,7 +73,7 @@ public:
 	bool print_console();
 	bool print_file(ofstream &fout);
 	void del(T el);
-	T find_min (Root* el);
+	T find_min(Root* el);
 private:
 	T D;
 	Root *l;

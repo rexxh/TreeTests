b0766a11f3a3832e0b18fc166d531de136332983
Balanar300hp <kudashov.vlad@yandex.ru>
2016-05-18 12:27:58 +0300

    Update tree.cpp

--- include/tree.cpp
+++ include/tree.cpp
@@ -41,29 +41,29 @@ void Tree<T>::Root::del(T x) {
 if ((x == D) && (!l) && (!r)) {Root* root= nullptr; throw Deleted(); }
 if ((x == D) && (!l)) {
 D = r->D;
-if (r->l) l = r->l; else { delete[] l; l = nullptr; }
-if (r->r) r = r->r; else { delete[] r; r = nullptr; }
+if (r->l) l = r->l; else { delete l; l = nullptr; }
+if (r->r) r = r->r; else { delete r; r = nullptr; }
 return;
 }
 if ((x == D) && (!r)) {
 D = l->D;
-if (l->r) r = l->r; else { delete[] r; r = nullptr; }
-if (l->l) l = l->l; else { delete[] l; l = nullptr; }
+if (l->r) r = l->r; else { delete r; r = nullptr; }
+if (l->l) l = l->l; else { delete l; l = nullptr; }
 return;
 }
 if (x < D) {
-if ((l->D == x) && (!(l->r)) && (!(l->l))) { delete[] l; l = nullptr; return; }
-if ((l->D == x) && (l->l) && (l->r)) { l->D = find_min(l->r); if (l->r->D != find_min(l->r)) l->r->del(find_min(l->r)); else { delete[] l->r; l->r = nullptr; } return; }
+if ((l->D == x) && (!(l->r)) && (!(l->l))) { delete l; l = nullptr; return; }
+if ((l->D == x) && (l->l) && (l->r)) { l->D = find_min(l->r); if (l->r->D != find_min(l->r)) l->r->del(find_min(l->r)); else { delete l->r; l->r = nullptr; } return; }
 else; l->del(x);
 return;
 }
 if (x > D) {
-if ((r->D == x) && (!(r->r)) && (!(r->l))) { delete[] r; r = nullptr; return; }
-if ((r->D == x) && (r->l) && (r->r)) { r->D = find_min(r->r); if (r->r->D != find_min(r->r)) r->r->del(find_min(r->r)); else { delete[] r->r; r->r = nullptr; } return; }
+if ((r->D == x) && (!(r->r)) && (!(r->l))) { delete r; r = nullptr; return; }
+if ((r->D == x) && (r->l) && (r->r)) { r->D = find_min(r->r); if (r->r->D != find_min(r->r)) r->r->del(find_min(r->r)); else { delete r->r; r->r = nullptr; } return; }
 else r->del(x);
 return;
 }
-if ((x == D) && (l) && (r)) { D = find_min(r); if (r->D != find_min(r)) r->del(find_min(r)); else { delete[]r; r = nullptr; } return; }
+if ((x == D) && (l) && (r)) { D = find_min(r); if (r->D != find_min(r)) r->del(find_min(r)); else { delete r; r = nullptr; } return; }
 }
 
 template <class T>
@@ -88,7 +88,7 @@ template <class T>
 bool Tree<T>::Root::print_console() {
 if (this != nullptr) {
 if (l != nullptr) l->print_console();
-cout « D « " ";
+cout << D << " ";
 if (r != nullptr) r->print_console();
 return true;
 }
@@ -99,7 +99,7 @@ bool Tree<T>::Root::print_file(ofstream &fout) {
 if (this != nullptr) {
 if (fout.is_open()) {
 if (l != nullptr) l->print_file(fout);
-fout « D « " ";
+fout << D << " ";
 if (r != nullptr) r->print_file(fout);
 }
 return true;

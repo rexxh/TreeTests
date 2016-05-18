88c4c00ecd60cd58246d1ced80efce4b241a430c
Balanar300hp <kudashov.vlad@yandex.ru>
2016-05-19 02:26:22 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -277,25 +277,27 @@ SCENARIO("Add_int999", "[add_l]"){
   REQUIRE(tree.Search(3));
 }
 
+
 SCENARIO("BST init with initializer list", "[init]") {
 	Tree<int> tree {8};
 	REQUIRE( tree.size() == 1 );
 }
 
 SCENARIO("BST search inserted element", "[search]") {
-	Tree<int> tree {8, 4, 3};
+	BinarySearchTree<int> tree  {8, 4, 3};
 	REQUIRE( tree.Search(4) );
 }
 
 SCENARIO("BST search non inserted element", "[search]") {
-Tree<int> tree {8, 4, 3};
+	BinarySearchTree<int> tree  {8, 4, 3};
 	REQUIRE( !tree.Search(5) );
 }
- 
-SCENARIO("BST delete non inserted element", "[delete]") {
-	Tree<int> tree {8};
+
+/*SCENARIO("BST delete non inserted element", "[delete]") {
+	BinarySearchTree<int> tree = {8};
+	REQUIRE( !tree.remove(4) );
 	REQUIRE( !tree.isEmpty() );
-}
+}*/
 
 // 				  +----+
 // 				  |-08-|									           *
@@ -305,11 +307,9 @@ SCENARIO("BST delete non inserted element", "[delete]") {
 // 						 				    08
 // 						 				   ---->
 SCENARIO("BST delete root without children", "[delete]") {
-	Tree<int> tree {8};int O=0;
-try{ tree.del(8);}
-catch(Deleted &){O++;}
-REQUIRE(O==1);
-	REQUIRE( tree.isEmpty() );
+	BinarySearchTree<int> tree  {8};
+	REQUIRE( tree.del(8) );
+//	REQUIRE( tree.isEmpty() );
 }
 
 //                +----+                                              +----+
@@ -325,5 +325,124 @@ REQUIRE(O==1);
 //      +----+                              08
 //      | 03 |                             ---->
 //      +----+
+SCENARIO("BST delete root with one child", "[delete]") {
+	BinarySearchTree<int> tree  {8, 4, 3};
+	REQUIRE( tree.del(8) );
+	REQUIRE( tree == Tree<int>({4, 3}) );
+}
 
+//                +----+                                              +----+
+//                |-08-|                                              | 09 |
+//                +----+                                              +----+
+//                  /\                                                  /\
+//                 /  \                                                /  \
+//           +----+    +----+                                    +----+    +----+
+//           | 04 |    | 10 |                                    | 04 |    | 10 |
+//           +----+    +----+                                    +----+    +----+
+//             /         /\                                        /          \
+//            /         /  \                                      /            \
+//      +----+    +----+    +----+          08              +----+              +----+
+//      | 03 |    | 09 |    | 13 |         ---->            | 03 |              | 13 |
+//      +----+    +----+    +----+                          +----+              +----+
+//                            /                                                   /
+//                           /                                                   /
+//                     +----+                                              +----+
+//                     | 11 |                                              | 11 |
+//                     +----+                                              +----+
+//                        \                                                   \
+//                         \                                                   \
+//                          +----+                                              +----+
+//                          | 12 |                                              | 12 |
+//                          +----+                                              +----+
+SCENARIO("BST delete root with children", "[delete]") {
+	BinarySearchTree<int> tree {8, 4, 3, 10, 9, 13, 11, 12};
+	REQUIRE( tree.del(8) );
+	REQUIRE( tree == Tree<int>({9, 4, 3, 10, 13, 11, 12}) );
+}
 
+//                +----+                                              +----+
+//                | 08 |                                              | 08 |
+//                +----+                                              +----+
+//                  /\                                                  /\
+//                 /  \                                                /  \
+//           +----+    +----+                                    +----+    +----+
+//           | 04 |    | 10 |                                    | 04 |    | 10 |
+//           +----+    +----+                                    +----+    +----+
+//             /         /\                                                  /\
+//            /         /  \                                                /  \
+//      +----+    +----+    +----+          03                        +----+    +----+
+//      |-03-|    | 09 |    | 13 |         ---->                      | 09 |    | 13 |
+//      +----+    +----+    +----+                                    +----+    +----+
+//                            /                                                   /
+//                           /                                                   /
+//                     +----+                                              +----+
+//                     | 11 |                                              | 11 |
+//                     +----+                                              +----+
+//                        \                                                   \
+//                         \                                                   \
+//                          +----+                                              +----+
+//                          | 12 |                                              | 12 |
+//                          +----+                                              +----+
+SCENARIO("BST delete non root without children", "[delete]") {
+	Tree<int> tree  {8, 4, 3, 10, 9, 13, 11, 12};
+	REQUIRE( tree.del(3) );
+	REQUIRE( tree == Tree<int>({8, 4, 10, 9, 13, 11, 12}) );
+}
+
+//                +----+                                              +----+
+//                | 08 |                                              | 08 |
+//                +----+                                              +----+
+//                  /\                                                  /\
+//                 /  \                                                /  \
+//           +----+    +----+                                    +----+    +----+
+//           | 04 |    | 10 |                                    | 04 |    | 10 |
+//           +----+    +----+                                    +----+    +----+
+//             /         /\                                        /         /\
+//            /         /  \                                      /         /  \
+//      +----+    +----+    +----+          11              +----+    +----+    +----+
+//      | 03 |    | 09 |    | 13 |         ---->            | 03 |    | 09 |    | 13 |
+//      +----+    +----+    +----+                          +----+    +----+    +----+
+//                            /                                                   /
+//                           /                                                   /
+//                     +----+                                              +----+
+//                     |-11-|                                              | 12 |
+//                     +----+                                              +----+
+//                        \
+//                         \
+//                          +----+
+//                          | 12 |
+//                          +----+
+SCENARIO("BST delete non root with one child", "[delete]") {
+	BinarySearchTree<int> tree  {8, 4, 3, 10, 9, 13, 11, 12};
+	REQUIRE( tree.del(11) );
+	REQUIRE( tree == Tree<int>({8, 4, 3, 10, 9, 13, 12}) );
+}
+
+//                +----+                                              +----+
+//                | 08 |                                              | 08 |
+//                +----+                                              +----+
+//                  /\                                                  /\
+//                 /  \                                                /  \
+//           +----+    +----+                                    +----+    +----+
+//           | 04 |    |-10-|                                    | 04 |    | 11 |
+//           +----+    +----+                                    +----+    +----+
+//             /         /\                                        /         /\
+//            /         /  \                                      /         /  \
+//      +----+    +----+    +----+          10              +----+    +----+    +----+
+//      | 03 |    | 09 |    | 13 |         ---->            | 03 |    | 09 |    | 13 |
+//      +----+    +----+    +----+                          +----+    +----+    +----+
+//                            /                                                   /
+//                           /                                                   /
+//                     +----+                                              +----+
+//                     | 11 |                                              | 12 |
+//                     +----+                                              +----+
+//                        \
+//                         \
+//                          +----+
+//                          | 12 |
+//                          +----+
+SCENARIO("BST delete non root with children", "[delete]") {
+	BinarySearchTree<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
+	REQUIRE( tree.del(10) );
+	REQUIRE( tree == Tree<int>({8, 4, 3, 11, 9, 13, 12}) );
+}

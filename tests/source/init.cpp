9dcc9625700b1f8b36fef7456f003d6a119b4f41
rexxh <rexxh@mail.ru>
2016-05-20 01:10:13 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -13,10 +13,10 @@ tree.Insert(Root, 6, 100);
 tree.Insert(Root, 0, 200);
 tree.Insert(Root, -5, 800);
 tree.Insert(Root, 12, 44);
-REQUIRE(tmp = tree.Search(6));
-REQUIRE(tmp =tree.Search(0));
-REQUIRE(tmp =tree.Search(-5));
-REQUIRE(tmp =tree.Search(12));
+REQUIRE(tmp == tree.Search(6));
+REQUIRE(tmp ==tree.Search(0));
+REQUIRE(tmp ==tree.Search(-5));
+REQUIRE(tmp ==tree.Search(12));
 REQUIRE
 (tree.Search(11);
 catch(Element_not_found errorrr););

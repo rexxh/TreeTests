34368fbb2e46fb2004bde43221cd0b2018c9d013
rexxh <rexxh@mail.ru>
2016-05-20 01:16:37 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -8,15 +8,21 @@ using namespace std;
 SCENARIO("Search init", "[init]") {
 BST<int> tree(2, 1);
 Node<int>* tmp;
+int x=0; 
+int y=0;
 Node<int>* Root = tree.get_root();
 tree.Insert(Root, 6, 100);
 tree.Insert(Root, 0, 200);
 tree.Insert(Root, -5, 800);
 tree.Insert(Root, 12, 44);
+tree.Search(11);
+catch(Element_not_found errorrr) {x++;}
+tree.Search(1);
+catch(Element_not_found errorrr) {y++;}
 REQUIRE(tmp == tree.Search(6));
 REQUIRE(tmp ==tree.Search(0));
 REQUIRE(tmp ==tree.Search(-5));
 REQUIRE(tmp ==tree.Search(12));
-REQUIRE(tree.Search(11);catch(Element_not_found errorrr););
-REQUIRE(tree.Search(1);catch(Element_not_found errorrr););
+REQUIRE(x==1);
+REQUIRE(y==1);
 }
